75c6d453f369cabeca8b215afb2fd7dad5b96a06
terruno <haritz@terrezu.com>
2025-04-23 09:02:28 +0200

    clean

--- en/bodegas/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggle = document.getElementById('menu-toggle');
-    const menu = document.querySelector('nav ul');
-
-    toggle.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
-});
