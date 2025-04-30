7f5ce5f4e3e2a7ce16394491108cb7c27a0bf8de
terruno <haritz@terrezu.com>
2025-04-30 09:58:47 +0200

    Update20250430

--- es/js/cookies.js
+++ es/js/cookies.js
@@ -1,17 +1,16 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
-window.addEventListener("load", function(){
-    window.cookieconsent.initialise({
-      palette: {
-        popup: { background: "#000" },
-        button: { background: "#f1d600", text: "#000" }
-      },
-      theme: "classic",
-      position: "bottom",
-      content: {
-        message: "Este sitio web utiliza cookies técnicas para su funcionamiento.",
-        dismiss: "Aceptar",
-        link: "Leer más",
-        href: "politica-cookies.html"
-      }
-    });
+window.addEventListener("load", function () {
+  window.cookieconsent.initialise({
+    palette: {
+      popup: { background: "#000" },
+      button: { background: "#f1d600", text: "#000" }
+    },
+    theme: "classic",
+    position: "bottom",
+    content: {
+      message: "This website uses technical cookies for its operation.",
+      dismiss: "Accept",
+      link: "Learn more",
+      href: "politica-cookies.html"
+    }
   });
+});
