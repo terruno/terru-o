f05878287970d725b745965cb665fcc30ea0341e
terruno <haritz@terrezu.com>
2025-05-27 10:36:33 +0200

    update20250527_9

--- en/js/navbar.js
+++
@@ -1,9 +0,0 @@
-// Menú hamburguesa
-document.addEventListener('DOMContentLoaded', function () {
-    const toggle = document.getElementById('menu-toggle');
-    const menu = document.querySelector('nav.menu-central'); // este es el contenedor <nav>
-
-    toggle.addEventListener('click', () => {
-        menu.classList.toggle('active'); // ahora se alinea con tu CSS
-    });
-});
